7a1720b0397c2e56d0abae3105fdb9e590ce2a5f
kiril <kirill.mereshchenko@gmail.com>
2023-05-25 22:23:11 +0300

    dz 5

---
+++ dz_5/main.py
@@ -0,0 +1,17 @@
+import colorama
+
+#help(colorama)
+"""
+Back(method) - It change background 
+Autoreset(attribute) - 	It is used to reset the color and style after each line when the value of this argument is set to True.
+Strip(attribute) - It is used to remove the ANSI code from the output when the value of this argument is set to True.
+Convert(attribute) - It is used to convert the ANSI code of the output when the value of this argument is set to True.
+Wrap(attribute) - It is used to disable the overriding task when the value of this argument is set to False.
+"""
+help(colorama.initialise)
+"""
+colorama_text(method) - It change color
+reset_all(method) - It reset all color and style settings to their defaults.
+"""
+
+
