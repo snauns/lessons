4aa3865fe6a91f2448b7ffeea6327268b03ac50b
kiril <kirill.mereshchenko@gmail.com>
2023-05-26 20:28:07 +0300

    dz 6

---
+++ dz_6/main.py
@@ -0,0 +1,23 @@
+import kem as kem
+
+result = []
+
+
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a / b
+
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, tuple([]): 15, 8: 4}
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except Exception as ex:
+        print(ex)
+
+
+print(result)
