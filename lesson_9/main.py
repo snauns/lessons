11fa9dc2456442133f9b18ca789b8437503b747e
kiril <kirill.mereshchenko@gmail.com>
2023-09-03 14:09:23 +0300

    lesson9

---
+++ lesson_9/main.py
@@ -0,0 +1,27 @@
+#1
+'''
+import urllib.request as ur
+opener = ur.build_opener()
+response = opener.open("https://httpbin.org/get")
+print(response.read())
+'''
+#2
+'''
+import requests as r
+response = r.get("https://httpbin.org/get")
+print(response.content)
+'''
+from parsehtml import ParserHTLM
+selectedCurrency = int(input("Select currency: \n[EU - 1]\n[USD - 2]: "))
+amount = float(input("Enter amount in hrn: "))
+parser = ParserHTLM("https://bank.gov.ua/")
+isUSD = True
+symbol = "$"
+if selectedCurrency == 1:
+    isUSD = False
+    symbol = "€"
+parser.ParseNBU('value-full', 'small', isUSD)
+result = amount / parser.Result[0]
+print(f"amount - {amount} hrn\n"
+      f"price - {parser.Result[0]} {symbol}\n"
+      f"result - {result:.2f} {symbol}")
